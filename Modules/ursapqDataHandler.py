3c26b42c6ea914d4b5c998d63ab016e9cf763b55
ursapq <ursapq@expquant>
2023-02-07 09:33:11 +0100

    update

--- Modules/ursapqDataHandler.py
+++ Modules/ursapqDataHandler.py
@@ -123,12 +123,14 @@ class ursapqDataHandler:
         try:
             self.tofAccumulator[1] += newTof['data'].T[1] 
             self.accumulatorCount += 1
-            self.gmdAccumulator += newGmd.mean()
+            if config.Data_GmdNorm:
+                self.gmdAccumulator += newGmd.mean()
         except Exception as error:
             traceback.print_exc()
             self.tofAccumulator  = newTof['data'].T.copy() #Rest tof accumulator
             self.accumulatorCount = 1
-            self.gmdAccumulator = newGmd.mean() 
+            if config.Data_GmdNorm:
+                self.gmdAccumulator = newGmd.mean() 
         return True
                            
     def updateLaserTrace(self):
@@ -155,7 +157,7 @@ class ursapqDataHandler:
     def Tof2eV(self, tof, retard):
         ''' converts time of flight into ectronvolts '''
         # Constants for conversion:
-        s = 1.7
+        s = config.Data_BottleLenght
         m_over_e = 5.69
 
         # UNITS AND ORDERS OF MAGNITUDE DO CHECK OUT
@@ -234,9 +236,9 @@ class ursapqDataHandler:
               
                 self.status.data_evenAccumulator =  evenAcc 
                 self.status.data_oddAccumulator  =  oddAcc
-                self.status.data_gmdAccumulator  = self.gmdAccumulator
                 self.status.data_AccumulatorCount = self.accumulatorCount
-               
+                if config.Data_GmdNorm:
+                    self.status.data_gmdAccumulator  = self.gmdAccumulator
                 self.status.data_updateFreq = self.updateFreq
                 self.status.data_laserTrace = self.laserTrace
                 self.status.data_tofTrace   = self.tofTrace
